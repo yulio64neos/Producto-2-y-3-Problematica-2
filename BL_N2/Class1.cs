d5916b65f10fe284b34aa73898a761d197ad3de6
yulio64neos <julio64neos@gmail.com>
2022-07-19 20:43:15 -0500

    Escenario CAPAS DAL - BL - PRESENTACION

---
+++ BL_N2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL_N2
+{
+    public class Class1
+    {
+    }
+}
