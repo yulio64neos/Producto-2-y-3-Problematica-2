686575deaabd0e8fe033996be4855a45100ecc76
yulio64neos <julio64neos@gmail.com>
2022-07-19 22:07:54 -0500

    Se cambio el nombre de la clase de la capa DAL

--- DAL_N1/DatosL.cs
+++ DAL_N1/DatosL.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL_N1;
 
 namespace DAL_N1
 {
-    public class Class1
+    public class DatosL
     {
     }
 }
