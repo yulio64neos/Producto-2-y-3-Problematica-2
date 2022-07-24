6d73786661c08e95f01e28c57dd2d52c9fd82c84
yulio64neos <julio64neos@gmail.com>
2022-07-23 23:43:09 -0500

    Componentes filtrados

--- ScriptSqlPruebas/EliminarConstraints.sql
+++ ScriptSqlPruebas/EliminarConstraints.sql
@@ -110,4 +110,145 @@ select Componentes.nom_Componente as 'Componente', Marca.Marca from Componentes 
 where nom_Componente = 'ModeloCPU'
 
 
+select Marca, nom_Componente from Componentes inner join Marca on(Componentes.id_Componente = Marca.Id_Componente)
 
+select * from DiscoDuro
+select * from Componentes
+
+alter table DiscoDuro add Id_Marca int
+alter table DiscoDuro add constraint fk_marca_disco foreign key(Id_Marca) references Marca(Id_Marca)
+
+alter table DiscoDuro drop column Extra
+
+update DiscoDuro set DiscoDuro.Id_Marca = 5 where id_Disco = 6
+update DiscoDuro set DiscoDuro.Id_Marca = 4 where id_Disco = 7
+
+select Marca, nom_Componente from Componentes inner join Marca on(Componentes.id_Componente = Marca.Id_Componente)
+
+
+
+
+alter table Marca drop constraint fk_marca_componente
+alter table Marca drop column Id_Componente
+
+select * from Marca
+
+
+create table Marca_Componente(
+id_Marca int not null,
+id_Compo int not null,
+constraint fk_marca_t foreign key (id_Marca) references Marca(Id_Marca),
+constraint fk_compo_t foreign key (id_Compo) references Componentes(id_Componente)
+)
+
+select * from Marca
+select * from Componentes
+insert into Marca_Componente (id_Marca, id_Compo) 
+values
+(1, 7),
+(2, 7),
+(3, 1),
+(4, 1),
+(5, 2),
+(6, 2),
+(7, 3),
+(8, 3),
+(9, 4),
+(10, 4),
+(11, 5),
+(12, 5),
+(13, 6),
+(14, 6);
+
+select Marca.Id_Marca, Marca, nom_Componente from Marca inner join Marca_Componente
+ON (Marca.Id_Marca = Marca_Componente.id_Marca) inner join Componentes
+ON (Componentes.id_Componente = Marca_Componente.id_Compo)
+
+SELECT TipoDisco, nom_Componente, Marca 
+FROM DiscoDuro inner join Marca ON (Marca.Id_Marca = DiscoDuro.Id_Marca)
+inner join Marca_Componente  ON (Marca.Id_Marca = Marca_Componente.id_Marca)
+inner join Componentes ON (Marca_Componente.id_Compo = Componentes.id_Componente)
+
+
+alter table monitor add Id_Marca int
+alter table monitor add constraint fk_marca_monitor foreign key (Id_Marca) references Marca(Id_Marca)
+
+update monitor set monitor.Id_Marca = 7 where monitor.id_monitor = 4
+update monitor set monitor.Id_Marca = 8 where monitor.id_monitor = 5
+
+SELECT conectores, nom_Componente, Marca 
+FROM monitor inner join Marca ON (Marca.Id_Marca = monitor.Id_Marca)
+inner join Marca_Componente  ON (Marca.Id_Marca = Marca_Componente.id_Marca)
+inner join Componentes ON (Marca_Componente.id_Compo = Componentes.id_Componente)
+
+
+alter table teclado add Id_Marca int
+alter table teclado add constraint fk_marca_teclado foreign key (Id_Marca) references Marca(Id_Marca)
+
+select * from teclado
+
+update teclado set teclado.Id_Marca = 9 where teclado.id_teclado = 5
+update teclado set teclado.Id_Marca = 10 where teclado.id_teclado = 6
+
+
+SELECT conector, nom_Componente, Marca 
+FROM teclado inner join Marca ON (Marca.Id_Marca = teclado.Id_Marca)
+inner join Marca_Componente  ON (Marca.Id_Marca = Marca_Componente.id_Marca)
+inner join Componentes ON (Marca_Componente.id_Compo = Componentes.id_Componente)
+
+alter table mouse add Id_Marca int
+alter table mouse add constraint fk_marca_mouse foreign key (Id_Marca) references Marca(Id_Marca)
+
+select * from mouse
+
+update mouse set mouse.Id_Marca = 11 where mouse.id_mouse = 5
+update mouse set mouse.Id_Marca = 12 where mouse.id_mouse = 6
+
+SELECT conector, nom_Componente, Marca 
+FROM mouse inner join Marca ON (Marca.Id_Marca = mouse.Id_Marca)
+inner join Marca_Componente  ON (Marca.Id_Marca = Marca_Componente.id_Marca)
+inner join Componentes ON (Marca_Componente.id_Compo = Componentes.id_Componente)
+
+alter table Gabinete add Id_Marca int
+alter table Gabinete add constraint fk_marca_gabi foreign key (Id_Marca) references Marca(Id_Marca)
+
+select * from Gabinete
+
+update Gabinete set Gabinete.Id_Marca = 13 where Gabinete.id_Gabinete = 5
+update Gabinete set Gabinete.Id_Marca = 14 where Gabinete.id_Gabinete = 6
+
+SELECT TipoForma, nom_Componente, Marca 
+FROM Gabinete inner join Marca ON (Marca.Id_Marca = Gabinete.Id_Marca)
+inner join Marca_Componente  ON (Marca.Id_Marca = Marca_Componente.id_Marca)
+inner join Componentes ON (Marca_Componente.id_Compo = Componentes.id_Componente)
+
+alter table ModeloCPU add Id_Marca int
+alter table ModeloCPU add constraint fk_marca_CPU foreign key (Id_Marca) references Marca(Id_Marca)
+
+select * from ModeloCPU
+
+update ModeloCPU set ModeloCPU.Id_Marca = 1 where ModeloCPU.id_modcpu = 1
+update ModeloCPU set ModeloCPU.Id_Marca = 1 where ModeloCPU.id_modcpu = 2
+update ModeloCPU set ModeloCPU.Id_Marca = 1 where ModeloCPU.id_modcpu = 4
+update ModeloCPU set ModeloCPU.Id_Marca = 1 where ModeloCPU.id_modcpu = 6
+update ModeloCPU set ModeloCPU.Id_Marca = 1 where ModeloCPU.id_modcpu = 7
+update ModeloCPU set ModeloCPU.Id_Marca = 2 where ModeloCPU.id_modcpu = 8
+update ModeloCPU set ModeloCPU.Id_Marca = 2 where ModeloCPU.id_modcpu = 9
+update ModeloCPU set ModeloCPU.Id_Marca = 2 where ModeloCPU.id_modcpu = 10
+update ModeloCPU set ModeloCPU.Id_Marca = 2 where ModeloCPU.id_modcpu = 11
+update ModeloCPU set ModeloCPU.Id_Marca = 2 where ModeloCPU.id_modcpu = 12
+update ModeloCPU set ModeloCPU.Id_Marca = 2 where ModeloCPU.id_modcpu = 13
+update ModeloCPU set ModeloCPU.Id_Marca = 2 where ModeloCPU.id_modcpu = 14
+
+SELECT modeloCPU, nom_Componente, Marca 
+FROM ModeloCPU inner join Marca ON (Marca.Id_Marca = ModeloCPU.Id_Marca)
+inner join Marca_Componente  ON (Marca.Id_Marca = Marca_Componente.id_Marca)
+inner join Componentes ON (Marca_Componente.id_Compo = Componentes.id_Componente)
+
+select * from actualizacion
+
+sp_help DiscoDuro
+sp_help monitor
+sp_help teclado
+sp_help mouse
+sp_help Gabinete
